3a05c640781bd1068661b8603657c44129921c01
slyth11907 <blackhat.brandon@gmail.com>
2016-05-22 15:53:41 -0700

    Update Bitwise.asm

--- Code-Examples/Bitwise.asm
+++ Code-Examples/Bitwise.asm
@@ -10,26 +10,28 @@ _start:
   mov al, 0x10 ; this move 10 into al
   and al, 0x01 ; this performs an AND on al & 01 resulting in 0x00
 
-  and byte [var1], 0xaa
-  and word [var2], 0x1122
+  and byte [var1], 0xaa ; this performs an AND on 0xaa & 0xaa resulting in 0xaa
+  and word [var2], 0x1122 ; this performs an AND on 0x1122 & 0xbbcc resulting in 0x1100
   
   ; OR
   mov al, 0x10 ; this move 10 into al
   or al, 0x01 ; this performs an OR on al & 01 resulting in 0x11
   
-  or byte [var1], 0xaa
+  or byte [var1], 0xaa ; this performs an OR on 0xaa & 0xaa resulting in 0xaa
   
   mov eax, 0 ; this move 0 into eax
   or eax, 0x0 ; this performs an OR on eax & 0 resulting in 0x00000000
   
   ; XOR
-  xor dword [var3], 0x11223344
-  xor dword [var3], 0x11223344
+  ; xoring the sam value will produce 0, good thing to note if you want to avoid 0x0 in your source when compile to shellcode
+  xor dword [var3], 0x11223344 ; this performs an XOR on 11223344 & 11223344 resulting in 0x00000000
+  xor dword [var3], 0x11223344 ; this performs an XOR on 11223344 & 11223344 resulting in 0x00000000
   
   ; NOT
-  mov eax, 0xFFFFFFFF
-  not eax
-  not eax
+  mov eax, 0xFFFFFFFF ; moves 0xFFFFFFFF into eax
+  not eax ; this performs an NOT on eax resulting in 0x00000000
+  ; since NOT inverts the bits the above command 0's them out as they were all 1's and the following command brings them back to 1's
+  not eax ; this performs an NOT on eax resulting in 0x11111111
   
   
   ; exit the application
