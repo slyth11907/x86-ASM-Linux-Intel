25902597afd20bf6b0ca1bd7174d28ab34efe2af
slyth11907 <blackhat.brandon@gmail.com>
2016-05-22 19:58:16 -0700

    Update jmp-call-pop.asm

--- Code-Examples/ShellCode/jmp-call-pop.asm
+++ Code-Examples/ShellCode/jmp-call-pop.asm
@@ -2,7 +2,7 @@
 ; Author: Brandon Dennis
 
 ; Compiles shellcode no nulls
-; "\x31\xc0\xb0\x04\x31\xdb\xb3\x01\xb9\x9c\x90\x04\x08\x31\xd2\xb2\x13\xcd\x80\x31\xc0\xb0\x01\xcd\x80"
+; 
 
 global _start
 
@@ -11,6 +11,15 @@ section .text
 _start:
 
 ; We need to remove all null values from the shell code. Lines will be commented out
+
+	; we issue our jmp short to start jmp call pop
+	jmp short call_shellcode
+
+
+shellcode:
+
+	; we now use our pop to get the memory address of message
+	pop ecx
 
 	;mov eax, 0x4 
 	xor eax, eax ; this will 0 out eax so we only have to lead 4 into al instead of all 32bits
@@ -33,9 +42,9 @@ _start:
 	;mov ebx, 0x5 ; we can remove this entirly as it doesnt matter the exit code
 	int 0x80
 
+	
 
-
-section .data
-
+call_shellcode:
+	call shellcode
 	message: db "Hello World!", 0xA ; this makes the amount of bits as 13 for Hello World!
 ;	mlen	equ  $-message ; this is removed due to the length not fitting the entire register 
